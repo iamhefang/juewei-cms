6cd632accc4e7bb9fcfc5bb22019d154e8efa914
hefang <he@hefang.link>
2019-03-05 14:16:44 +0800

    完善Markdown编辑器

--- themes/simple-white/js/common.ts
+++ themes/simple-white/js/common.ts
@@ -3,12 +3,20 @@ const exports = {}, objMap = {
     'hefang-ui-react': 'HuiReact',
     'react': "React",
     'react-dom': 'ReactDOM',
-    'ace-builds': 'ace'
+    'ace-builds': 'ace',
+    'marked': 'marked',
+    'hefang-ui-mdeditor': "HuiMarkdownEditor",
+    'code-prettify': 'PR',
+    'katex': 'katex'
 };
 
 function require(name: string) {
     if (name in objMap) {
-        return window[objMap[name]];
+        const objName = objMap[name];
+        if (objName in window) {
+            return window[objName];
+        }
+        throw new Error(`对象${objName}不存在`);
     }
-    throw new Error(`对象${name}不存在`);
+    throw new Error(`库${name}未定义`);
 }
