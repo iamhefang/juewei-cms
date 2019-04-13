8fc0651174345c9e3755e87adec23e0c2ff2358d
hefang <he@hefang.link>
2019-04-13 09:32:34 +0800

    添加插件功能（未完成）

---
+++ application/link/hefang/cms/plugins/IPlugin.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hefang
+ * Date: 19-3-22
+ * Time: 上午10:49
+ */
+
+namespace link\hefang\cms\plugins;
+
+
+interface IPlugin
+{
+
+}
---
+++ application/link/hefang/cms/plugins/handlers/TemplateHandler.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hefang
+ * Date: 19-3-22
+ * Time: 上午10:58
+ */
+
+namespace link\hefang\cms\plugins\handlers;
+
+
+class TemplateHandler
+{
+
+}
